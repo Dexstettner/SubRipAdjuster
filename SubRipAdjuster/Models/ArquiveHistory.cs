b1f17165542f304cc7c9806e51c296fc62a6808d
Dexstettner <krochastettner2011@hotmail.com>
2021-03-22 02:00:39 -0300

    Display das labels do historico estarem padronizadas com o modelo

--- SubRipAdjuster/Models/ArquiveHistory.cs
+++ SubRipAdjuster/Models/ArquiveHistory.cs
@@ -27,13 +27,20 @@ namespace SubRipAdjuster.Models
 
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "ID")]
         public int Id { get => id; set => id = value; }
+
+        [Display(Name = "File Name")]
         public string ArquiveName { get => arquiveName; set => arquiveName = value; }
 
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm:ss}", ApplyFormatInEditMode = true)]
         [DataType(DataType.Date)]
+        [Display(Name = "Date")]
         public DateTime InsertDate { get => insertDate; set => insertDate = value; }
+
+        [Display(Name = "File")]
         public byte[] ArquiveFile { get => arquiveFile; set => arquiveFile = value; }
+        [Display(Name = "Milliseconds Shifted")] 
         public int Ms { get => ms; set => ms = value; }
     }
 }
